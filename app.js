f4d487ed94b0a443a9e732f0ed1082d20ee24f05
Jesus Bordones <jabordones92@gmail.com>
2018-01-12 14:39:37 -0500

    some media queries added

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 app.locals.title = "Chu's Portfolio";
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
